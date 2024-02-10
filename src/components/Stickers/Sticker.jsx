db3ce37ee4f5e797c7571c59e039149b50276e33
Safvanms <muhammedsafvan1ms@gmail.com>
2024-02-10 23:05:53 +0530

    New contents added

--- src/components/Stickers/Sticker.jsx
+++ src/components/Stickers/Sticker.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import './sticker.css'
-import keepCityClean from "../../assets/images/keepcityclean.png";
+import React from "react";
+import "./sticker.css";
+import KeepCityClean from "../../assets/images/keepcityclean.png";
 import GreenDot from "../../assets/images/Green-Dot.png";
 import water from "../../assets/images/water.png";
 import Earth from "../../assets/images/earth.png";
@@ -11,13 +11,17 @@ const Sticker = () => {
     <div>
       <div className="sticker_container GenFlex">
         <img src={GreenDot} alt="edible" className="sticker_icon" />
-        <img src={keepCityClean} alt="keep_city_clean" className="sticker_icon" />
+        <img
+          src={KeepCityClean}
+          alt="keep_city_clean"
+          className="sticker_icon"
+        />
         <img src={water} alt="save_water" className="sticker_icon" />
         <img src={Earth} alt="save_earth" className="sticker_icon" />
         <img src={Blood} alt="donate_blood" className="sticker_icon blood" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sticker
+export default Sticker;
