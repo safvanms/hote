576f21e3e65b73745c977b91a3bd817dc9b36a0a
Safvanms <muhammedsafvan1ms@gmail.com>
2024-02-18 16:36:16 +0530

    Contents added

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,23 +1,23 @@
-import React from "react";
-import "./contacts.css";
-import  data  from "../../data.json";
+// import React from "react";
+// import "./contacts.css";
+// import  data  from "../../data.json";
 
 
-const Contacts = () => {
-  const mapData = data.data
-  console.log(mapData)
-  return (
-    <div className="contacts_container GenFlex">
-      {mapData.map(({ name, item, image }) => (
-        <div key={item}>
-          {console.log(image)}
-          <p>{item}</p>
-          <h2>{name}</h2>
-          <img src={require(`${image}`)} alt="item" style={{width:"250px"}}/>
-        </div>
-      ))}
-    </div>
-  );
-};
+// const Contacts = () => {
+//   const mapData = data.data
+//   console.log(mapData)
+//   return (
+//     <div className="contacts_container GenFlex">
+//       {mapData.map(({ name, item, image }) => (
+//         <div key={item}>
+//           {console.log(image)}
+//           <p>{item}</p>
+//           <h2>{name}</h2>
+//           <img src={require(`${image}`)} alt="item" style={{width:"250px"}}/>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
 
-export default Contacts;
+// export default Contacts;
