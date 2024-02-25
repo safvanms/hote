f6933b218f1f3916caedc8c9e99ca839329381db
Safvanms <muhammedsafvan1ms@gmail.com>
2024-02-26 01:02:13 +0530

    menus added through the contentful CMS

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -17,15 +17,13 @@ const Menu = () => {
     //eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  console.log(menus);
-
   return (
     <div div className="menu_page">
       <div className="menu_container ">
         <div className="blur in-blur1 menu_blur1"></div>
         <h1>menú de hoté</h1>
 
-        {menus.length > 0 && (
+        {menus.length > 0 ? (
           <div className="menu GenFlex">
             <div className="menu_image GenFlex">
               <h2>hoté beverages</h2>
@@ -48,8 +46,11 @@ const Menu = () => {
               </ul>
             </div>
           </div>
+        ) : (
+          <div className="GenFlex" style={{height:"50vh"}}>
+            <div>Please wait... Your menu is on the way !</div>
+          </div>
         )}
-
       </div>
       <div className="home_return GenFlex">
         <Link to="/" style={{ textDecoration: "none" }}>
